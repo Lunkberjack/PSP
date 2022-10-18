d252dfe6e8767d469ba8235d06a2633975a7c38c
Lunkubuntu <l.le.mi.lunk15@gmail.com>
2022-10-18 23:27:15 +0200

    Palíndromas hechas
    ඞ

--- JavaProcesos/src/main/act1_14/PasaFicheros.java
+++ JavaProcesos/src/main/act1_14/PasaFicheros.java
@@ -7,46 +7,65 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class PasaFicheros {
-	private static final int ESCAPE=27;
 	public static void main(String[] args) {
-		int exitVal=0;
+		int exitVal = 0;
 		Scanner sc = new Scanner(System.in);
-		String cadena="go";
+		String cadena = "go";
 		try {
-			//se crea un fichero de texto a partir de las entradas
-			//introducidas por consola
+			// Se crea un fichero de texto a partir de las entradas
+			// introducidas por consola:
 			PrintWriter pw =
-					new PrintWriter(new FileWriter("./src/main/act1_14/datos.txt"));
+					new PrintWriter(new FileWriter("./src/main/act1_14/todas.txt"));
+			ProcessBuilder pb =
+					new ProcessBuilder("java", "main.act1_14.RecibeFicheros");
+
+			// Directorio trabajo:
+			pb.directory(new File("/home/lucia/PSP/JavaProcesos/bin"));
 			while(cadena.length() > 1 && cadena.charAt(0) != 0) {
 				System.out.println("Introduce una cadena palíndroma o no, "
-						+ "o vacía:(0 para terminar)");
+						+ "o vacía (0 para terminar): ");
 				cadena = sc.nextLine();
 				pw.println(cadena);
+
+				// Redirección del flujo de entrada del proceso del programa
+				// al que se llama (las palabras introducidas por teclado aquí):
+				pb.redirectInput(
+						new File("./src/main/act1_14/todas.txt"));
+				pb.redirectOutput(
+						new File("./src/main/act1_14/palindromas.txt"));
+				pb.redirectError(
+						new File("./src/main/act1_14/noPalindromas.txt"));
 			}
 			sc.close();
 			pw.close();
-			ProcessBuilder pb =
-					new ProcessBuilder("java", "main.act1_14.RecibeFicheros");
-			//directorio de trabajo
-			pb.directory(
-					new File("/home/lucia/PSP/JavaProcesos/bin"));
-			//redirección del flujo de entrada del proceso del programa
-			//al que se llama (las palabras introducidas por teclado aquí)
-			pb.redirectInput(
-					new File("./src/main/act1_14/datos.txt"));
-			//redirección del flujo de salida del proceso del programa al 
-			//que se llama
-			pb.redirectOutput(
-					new File("./src/main/act1_14/datosProcesados.txt"));
-			//ejecución del proceso
+			// Ejecución de RecibeFicheros:
 			Process process = pb.start();
-			//se espera para el valor retornado
 			exitVal = process.waitFor();
-		} catch (IOException | InterruptedException e) {
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//se muestra el resultado de la operación
+		// Se muestra el valor devuelto por la operación y se sale con él:
 		System.out.println("Terminado con valor:" + exitVal);
 		System.exit(exitVal);
+	}
+	/**
+	 * Una palabra es palíndroma si se lee de igual manera hacia izquierda
+	 * y derecha (ANA, SOMOS, SAAS)
+	 * @param pal
+	 * @return
+	 */
+	public static boolean esPalindroma(String pal) {
+		boolean res = false;
+		// StringBuilder para que sea más sencillo invertirla:
+		StringBuilder aux = new StringBuilder(pal);
+		// Conversión a String para poder usar equals():
+		if(String.valueOf(aux.reverse()).equals(pal)) {
+			res = true;
+		}
+		return res;
 	}
 }
--- JavaProcesos/src/main/act1_14/RecibeFicheros.java
+++ JavaProcesos/src/main/act1_14/RecibeFicheros.java
@@ -6,22 +6,22 @@ import java.io.InputStreamReader;
 
 public class RecibeFicheros {
 	public static void main(String[] args) {
-		//obtención de un lector a partir del flujo de entrada
-		//estándar (definido por el proceso del programa que llama
-		//a éste o bien por la entrada por consola)
+		// Obtención de un lector a partir del flujo de entrada
+		// estándar (definido por el proceso del programa que llama
+		// a éste o bien por la entrada por consola):
 		BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
 		String cadena = "";
-		int linea = 0;
 		int exitVal = 0;
 		try {
-			//lectura del flujo de entrada estándar hasta que no haya nada 
-			//que leer
+			// Lectura del flujo de entrada estándar hasta que no quede nada 
+			// para leer:
 			while(bReader.ready()) {
 				cadena = bReader.readLine();
-				//se escribe en el fichero definido por la redirección
-				//del programa que llama éste
-				System.out.println("línea="+ ++linea + "tamaño=;" + cadena.length()
-				+ ";espacios=" + (cadena.split(" ").length-1));
+				if(PasaFicheros.esPalindroma(cadena)) {
+					System.out.println("La palabra " + cadena + " es palíndroma.");
+				} else {
+					System.err.println("La palabra " + cadena + " no es palíndroma.");
+				}
 			}
 		} catch (IOException e) {
 			exitVal = -1;
